8f36b8da3369fcc76d53dda4bc86e063dcce4be1
Axmed-Maxamed <96660752+Axmed-Maxamed@users.noreply.github.com>
2022-08-05 23:33:15 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+document.getElementById('image').addEventListener('click', (event) => {
+  event.target.setAttribute("src", 'https://images.unsplash.com/photo-1568702846914-96b305d2aaeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80');
+
+});
